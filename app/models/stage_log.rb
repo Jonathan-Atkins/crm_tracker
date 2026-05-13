36b455157251ad75d01dd77d8f7762825e24644d
Jonathan Atkins <jonathanjatkins89@gmail.com>
2026-05-13 10:44:33 -0700

    Enhance StageLog validations and set up CustomersController and request specs

--- app/models/stage_log.rb
+++ app/models/stage_log.rb
@@ -1,6 +1,6 @@
 class StageLog < ApplicationRecord
   belongs_to :customer
 
-  validates :from_stage, presence: true
-  validates :to_stage, presence: true
+  validates :from_stage, presence: true , inclusion: { in: Customer.stages.values}
+  validates :to_stage, presence: true, inclusion: { in: Customer.stages.values}
 end
