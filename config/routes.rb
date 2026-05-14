9dcbfc7dfb7b32f8f5f7d37d3d2d4a27ed5f65e1
Jonathan Atkins <jonathanjatkins89@gmail.com>
2026-05-14 09:16:12 -0700

    Update customers resource routes to include all actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :customers, only: [:index, :show, :create, :update, :destroy] do
+      resources :customers do
         patch :move_stage, on: :member
       end
     end
